d72b94e83dbe758e32aefcb12c4a40b09354af08
My name <jibektoktogulova30@gmail.com>
2024-01-08 14:29:56 -0800

    Escape Sequences practice is done

---
+++ src/day03_variables/EscapeSequencesPractice.java
@@ -0,0 +1,14 @@
+package day03_variables;
+
+public class EscapeSequencesPractice {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("My favorite programming is \"Java\"\n\tJava is an \"object oriented\" programming language");
+
+
+
+
+    }
+}
